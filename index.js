ff443fe9a2d30218b6f33c79707d32800288bade
cou9 <y4yasertalha@gmail.com>
2022-12-14 08:31:28 +0530

    direct sms background handler

--- index.js
+++ index.js
@@ -2,22 +2,29 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, NativeModules} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
+var DirectSms = NativeModules.DirectSms;
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-    console.log("remoteMessage");
-    console.log(remoteMessage);
-  });
-  
-  function HeadlessCheck({isHeadless}) {
-    if (isHeadless) {
-      // App has been launched in the background by iOS, ignore
-      return null;
-    }
-    return <App />;
+  console.log('remoteMessage');
+  console.log(remoteMessage);
+  const {message, phone} = remoteMessage.data;
+  if (message && phone) {
+    DirectSms.sendDirectSms(phone, message);
+    console.log(message);
+    console.log(phone);
   }
+});
+
+function HeadlessCheck({isHeadless}) {
+  if (isHeadless) {
+    // App has been launched in the background by iOS, ignore
+    return null;
+  }
+  return <App />;
+}
 
 AppRegistry.registerComponent(appName, () => HeadlessCheck);
