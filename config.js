2e183960233eea2ed3115f25154a64d9571ce0a1
cou9 <y4yasertalha@gmail.com>
2023-01-07 17:47:11 +0530

    base url

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://localhost:5000';
+export const baseUrl = 'https://notifybot.netlify.app/';
 export const webClientId =
   '1022946711972-jeuqgqa05mjbbpa1essnnmhvl9c88bu8.apps.googleusercontent.com';
